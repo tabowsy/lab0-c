4cda03592568e6d5d937a61d4c898cfc349bba6d
Oscar Wu <tabowsy@gmail.com>
2022-01-05 21:27:07 +0800

    Add hello function
    new a function called do_hello in console.c, and hello interface in
    qtest.c

--- console.h
+++ console.h
@@ -81,4 +81,6 @@ bool run_console(char *infile_name);
 /* Callback function to complete command by linenoise */
 void completion(const char *buf, linenoiseCompletions *lc);
 
+bool do_hello(int argc, char *argv[]);
+
 #endif /* LAB0_CONSOLE_H */

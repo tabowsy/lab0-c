53a7c77851ce6f0462da290617134831df150661
Oscar Wu <tabowsy@gmail.com>
2022-01-07 16:26:11 +0800

    Modify console.[ch] to meet Address Sanitizer
    Change static echo param from bool to int in console.c
    Change simulation param from bool to int in console.c and extern in
    console.h
    Check by "make SANITIZER=1" and in ./qtest run help

--- console.c
+++ console.c
@@ -18,7 +18,7 @@
 #include "report.h"
 
 /* Some global values */
-bool simulation = false;
+int simulation = 0;
 static cmd_ptr cmd_list = NULL;
 static param_ptr param_list = NULL;
 static bool block_flag = false;
@@ -56,7 +56,7 @@ static int fd_max = 0;
 /* Parameters */
 static int err_limit = 5;
 static int err_cnt = 0;
-static bool echo = 0;
+static int echo = 0;
 
 static bool quit_flag = false;
 static char *prompt = "cmd> ";
